9961f55a371c3cec3db582248a14aa2614460b64
threadexio <pzarganitis@gmail.com>
2025-07-09 15:12:55 +0300

    remove unused dependencies

--- src/hardware_addr.rs
+++ src/hardware_addr.rs
@@ -45,13 +45,3 @@ impl FromStr for HardwareAddr {
         Ok(Self(octets))
     }
 }
-
-impl<'de> serde::Deserialize<'de> for HardwareAddr {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        s.parse().map_err(serde::de::Error::custom)
-    }
-}
