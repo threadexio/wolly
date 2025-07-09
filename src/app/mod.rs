75f7154f86c20366ac442afa8a0be6d4bea9c022
threadexio <pzarganitis@gmail.com>
2025-07-09 11:17:47 +0300

    remove tunnel.rs

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,17 +4,16 @@ use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 
 use eyre::{Context, Result, bail};
+use tokio::net::TcpListener;
 
 use crate::config::Config;
 use crate::config::address::Port;
 use crate::signal::Signals;
 
 mod mapping;
-mod tunnel;
 mod upstream;
 
 use self::mapping::{Mapping, MappingKind};
-use self::tunnel::Tunnels;
 use self::upstream::{ConnectOpts, Upstream};
 
 #[derive(Debug)]
@@ -104,10 +103,11 @@ impl App {
         let mut signals = Signals::new().context("failed to register signal handlers")?;
 
         for mapping in self.mappings.iter().cloned() {
-            let tunnels = Tunnels::new(&self);
+            let spawn_tunnel =
+                async |from, to, opts| spawn_tunnel(from, to, opts, Arc::clone(&self)).await;
 
             match mapping.kind {
-                MappingKind::OneToOne { from, to } => tunnels.spawn(from, to, mapping.opts).await?,
+                MappingKind::OneToOne { from, to } => spawn_tunnel(from, to, mapping.opts).await?,
 
                 MappingKind::ManyToOne {
                     from_ip,
@@ -117,7 +117,7 @@ impl App {
                     for port in from_ports {
                         let from = SocketAddr::new(from_ip, port);
 
-                        tunnels.spawn(from, to, mapping.opts.clone()).await?
+                        spawn_tunnel(from, to, mapping.opts.clone()).await?
                     }
                 }
 
@@ -132,7 +132,7 @@ impl App {
                         let from = SocketAddr::new(from_ip, from_port_range_start + i);
                         let to = SocketAddr::new(to_ip, to_port_range_start + i);
 
-                        tunnels.spawn(from, to, mapping.opts.clone()).await?;
+                        spawn_tunnel(from, to, mapping.opts.clone()).await?;
                     }
                 }
             }
@@ -142,4 +142,54 @@ impl App {
         info!("exiting...");
         Ok(())
     }
+}
+
+async fn spawn_tunnel(
+    from: SocketAddr,
+    to: SocketAddr,
+    opts: ConnectOpts,
+    app: Arc<App>,
+) -> Result<()> {
+    let listener = TcpListener::bind(from)
+        .await
+        .with_context(|| format!("failed to bind listener on {from}"))?;
+
+    tokio::spawn(async move {
+        loop {
+            let (a, addr) = match listener.accept().await {
+                Ok(x) => x,
+                Err(_) => continue,
+            };
+
+            let opts = opts.clone();
+            let app = Arc::clone(&app);
+            tokio::spawn(async move {
+                let span = error_span!("tunnel", from = addr.to_string(), to = to.to_string());
+                let _enter = span.enter();
+
+                info!("connected");
+
+                let upstream = app
+                    .upstream
+                    .get(&to.ip())
+                    .expect("upstream should be known");
+
+                let r: Result<()> = async move {
+                    let mut a = a;
+                    let mut b = upstream.connect(to.port(), &opts).await?;
+
+                    let _ = tokio::io::copy_bidirectional(&mut a, &mut b).await;
+                    info!("disconnected");
+                    Ok(())
+                }
+                .await;
+
+                if let Err(e) = r {
+                    error!("{e:#}");
+                }
+            });
+        }
+    });
+
+    Ok(())
 }
