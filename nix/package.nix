f21715b52d761f6a673ddc14d8d9c4692a3be6b1
threadexio <pzarganitis@gmail.com>
2026-02-02 01:54:18 +0200

    remove left over lib.trace

--- nix/package.nix
+++ nix/package.nix
@@ -1,42 +1,32 @@
 { rustPlatform
-, rustToolchain
-, stdenv
 , lib
 , ...
 }:
 
 let
   cargoManifest = lib.importTOML ../Cargo.toml;
-
-  x = { }
-    // (lib.trace "build platform: ${stdenv.buildPlatform.config}" { })
-    // (lib.trace "host platform: ${stdenv.hostPlatform.config}" { })
-    // (lib.trace "toolchain 1: ${toString rustToolchain}" { })
-    # // (lib.trace "toolchain 2: ${toString rustToolchain'}" {})
-  ;
 in
 
-rustPlatform.buildRustPackage
-  {
-    pname = cargoManifest.package.name;
-    version = cargoManifest.package.version;
+rustPlatform.buildRustPackage {
+  pname = cargoManifest.package.name;
+  version = cargoManifest.package.version;
 
-    src = with lib.fileset; toSource {
-      root = ../.;
-      fileset = unions [
-        ../.cargo
-        ../src
-        ../Cargo.lock
-        ../Cargo.toml
-      ];
-    };
+  src = with lib.fileset; toSource {
+    root = ../.;
+    fileset = unions [
+      ../.cargo
+      ../src
+      ../Cargo.lock
+      ../Cargo.toml
+    ];
+  };
 
-    cargoLock.lockFile = ../Cargo.lock;
+  cargoLock.lockFile = ../Cargo.lock;
 
-    meta = with lib; {
-      inherit (cargoManifest.package) description homepage;
-      license = licenses.gpl3Only;
-      mainProgram = "wolly";
-      platforms = platforms.all;
-    };
-  } // x
+  meta = with lib; {
+    inherit (cargoManifest.package) description homepage;
+    license = licenses.gpl3Only;
+    mainProgram = "wolly";
+    platforms = platforms.all;
+  };
+}
